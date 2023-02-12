03078ce1cd22979c102230b85d07d96172cce37c
unknown <guy.bartkus@gmail.com>
2023-02-12 07:27:20 -0800

    Forcebit shader code

--- client/glsl/forcebits/vs.glsl
+++ client/glsl/forcebits/vs.glsl
@@ -1,14 +1,13 @@
 #version 300 es
 
-#define PI_Half 1.5707963267
-#define PI 3.1415926538
-#define PI2 6.2831853071
+#define COMB_RADIUS 11.0
 
 in vec2 aPos;
 in float aDir;
 in int aProp;
 
 uniform vec2 screenDims;
+uniform float universeSize;
 uniform float speed;
 uniform int electronCount;
 uniform sampler2D electronTex;
@@ -31,18 +30,17 @@ vec2 fromAngle(float rads) {
 
 void main() {
     int checkCollide = (aProp >> 2) & 1;
+    int type = (aProp >> 3) & 1;
 
     if(checkCollide == 0) {
         oPos = aPos;
         oDir = aDir;
         oProp = aProp;
         oColor = vec3(0.0, 0.0, 0.0);
-        
-        return;
-    }
+        gl_PointSize = 0.0;
+        gl_Position = toClipSpace(aPos);
 
-    for(int i = 0; i < electronCount; i++) {
-        vec2 ePos = texelFetch(electronTex, ivec2(i, 0), 0).xy;
+        return;
     }
 
     if(type == 1) {
@@ -50,4 +48,42 @@ void main() {
     } else {
         oColor = vec3(0.2157, 0.3921, 1);
     }
+
+    vec2 center = screenDims/2.0;
+    int newPos = aPos;
+    int newProp = aProp;
+
+    if(distance(aPos, center) > universeSize) {
+        newProp ^= (1 << 0); // Respawn
+
+        oPos = aPos;
+        oDir = aDir;
+        oProp = newProp;
+        gl_PointSize = 0.0;
+        gl_Position = toClipSpace(aPos);
+
+        return;
+    }
+
+    for(int i = 0; i < electronCount; i++) {
+        vec2 ePos = texelFetch(electronTex, ivec2(i, 0), 0).xy;
+
+        if(distance(aPos, ePos) <= COMB_RADIUS) {
+            newProp |= (i << 4); // Encode electron ID
+            newProp ^= (1 << 1); // Collided
+
+            break;
+        }
+    }
+
+    vec2 fbVel = fromAngle(aDir);
+
+    newPos = newPos+(fbVel*speed);
+
+    oPos = newPos;
+    oDir = aDir;
+    oProp = newProp;
+
+    gl_PointSize = 2.0;
+    gl_Position = toClipSpace(newPos);
 }
